ffb42a459716489321b7536ea53f974656964b46
Rohmad <rohmad37@ymail.com>
2020-06-19 11:22:03 +0700

    Exercise Jumat 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let kalimat = "halo ini adalah perubahan huruf awalan kecil ke besar";
 
 String.prototype.changeTitle = function () {
   this.includes(" ");
-  let sentence = this.toLowerCase().split(" ");
+  let sentence = this.split(" ");
 
   for (let i = 0; i < sentence.length; i++) {
     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
